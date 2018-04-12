c76c33aa08d5a18a10b5b09ec3dc33e34bb730ad
Natalie Megan <nataliz@natalies-macbook-air.local>
2018-04-12 13:55:09 +0800

    Initial Commit

---
+++ Tabbed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tabbed
+//
+//  Created by Nataliz on 12/4/18.
+//  Copyright © 2018 Natalie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
